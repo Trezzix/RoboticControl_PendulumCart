4a3f472e738f79856d019c37b9ceb43cc2696395
Thomas Lønne Stiansen <102665421+Trezzix@users.noreply.github.com>
2025-10-16 11:51:01 +0200

    Files (guide-demo) added from laptop pre-computer switch

---
+++ Demos/quarc_read_write_demo_TLS_quarc_win64/quarc_read_write_demo_TLS_types.h
@@ -0,0 +1,30 @@
+/*
+ * quarc_read_write_demo_TLS_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "quarc_read_write_demo_TLS".
+ *
+ * Model version              : 14.24
+ * Simulink Coder version : 9.8 (R2022b) 13-May-2022
+ * C source code generated on : Thu Oct 16 10:36:47 2025
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_quarc_read_write_demo_TLS_types_h_
+#define RTW_HEADER_quarc_read_write_demo_TLS_types_h_
+
+/* Parameters (default storage) */
+typedef struct P_quarc_read_write_demo_TLS_T_ P_quarc_read_write_demo_TLS_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_quarc_read_write_demo_T RT_MODEL_quarc_read_write_dem_T;
+
+#endif                       /* RTW_HEADER_quarc_read_write_demo_TLS_types_h_ */
