c5c87d0774e1ebdc7173fefb0c9abfd446a7af82
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2025-11-03 08:35:41 +0100

    Added RL-design test and supplements from Bonerz & Matt

---
+++ Matlab/cart_rootLocusDesign.m
@@ -0,0 +1,63 @@
+clc; clear; close all;
+
+%% Chosen parameters
+
+weight = true;
+
+%% Load constant conversion parameters
+
+% from radians to degrees
+K_R2D = 180 / pi;
+% from degrees to radians
+K_D2R = 1 / K_R2D;
+% from Inch to Meter
+K_IN2M = 0.0254;
+% from Meter to Inch
+K_M2IN = 1 / K_IN2M;
+% from rad/s to RPM
+K_RDPS2RPM = 60 / ( 2 * pi );
+% from RPM to rad/s
+K_RPM2RDPS = 1 / K_RDPS2RPM;
+% from oz-force to N
+K_OZ2N = 0.2780139;
+% from N to oz-force
+K_N2OZ = 1 / K_OZ2N;
+
+%% Load parameters
+R_m = 2.6; % [ohm] Motor armature resistance
+L_m = 180e-6; % [H] Motor armature Inductance
+k_t = 1.088 * K_OZ2N * K_IN2M; % [Nm/A] Motor torque constant
+eta_m = 1; % [ (Tm*w) / (Vm * Im) ] Motor electro-mechanical efficiency
+k_m = 0.804e-3 * K_RDPS2RPM; % [Vs/rad] Motor back-EMF constant
+J_m = 5.523e-5 * K_OZ2N * K_IN2M; % [kg*m^2] Rotor inertia
+M_c2 = 0.57; % [kg] IP02 Cart mass w/3 cable connectors
+M_w = 0.37; % [kg] Cart weight
+K_g = 3.71; % [-] Internal gear ratio (planetary gearbox)
+eta_g = 1; % [-] Internal gear ratio efficiency (planetary gearbox)
+N_mp = 24; % [-] Number of teeth: cart motor pinion
+r_mp = 0.5 / 2 * K_IN2M; % [m] Motor pinion radius
+N_pp = 56; % [-] Number of teeth: cart position pinion
+r_pp = 1.167 /2 * K_IN2M; % [m] Position pinion radius
+P_r = 1e-2 / 6.01; % [m/teeth] Rack pitch
+T_c = 0.814; % [m] Cart travel
+
+%% Constants
+
+% Mass of cart
+if weight % with weight
+    M = M_w + M_c2;
+    B_c = 5.4; % [Nm*s/rad]
+else % without weight
+    M = M_c2;
+    B_c = 4.3; % [Nm*s/rad]
+end
+% Actuator Gain
+Am = ( eta_g * K_g * eta_m * k_t ) / ...
+            ( r_mp * R_m );
+% Equivalent Damping
+B_eq = ( (eta_g * K_g^2 * eta_m * k_t * k_m) + (B_c * r_mp^2 * R_m) ) / ...
+            ( r_mp * R_m );
+
+% Transfer Function
+K = Am/B_eq;
+tau = M/B_eq;
---
+++ Matlab/tests/test_EM.m
@@ -0,0 +1,47 @@
+% from Emanuel Matt 2025 (copied via WhatsApp message)
+
+clear all
+s=tf('s');
+
+% additional zero 
+z_c=0.5; 
+% continuous-time transfer function with the additional zero
+sys_c = (s + z_c) / s;
+
+%system definition
+sys_mL = 1/(s+2.071);
+
+ts = 5; % settlingtime in s
+Mp= 0.02; % overshoot (0.2 = 20%)
+D=-log(Mp)/sqrt(pi()^2+log(Mp)^2);  % damping
+w0=4/(D*ts);
+
+
+figure;
+% stability area
+theta = acos(D);    
+x = linspace(-2.5, 0, 100);
+y1 = tan(theta)*x;  
+y2 = -tan(theta)*x;
+plot(x, y1, '--b', x, y2, '--b', 'LineWidth', 1.5);
+text(-2, 0.5, 'stability area', 'Color', 'blue'); 
+hold on
+
+% root locus
+rlocus(sys_mL*sys_c);
+sgrid(D, w0);
+[K, poles] = rlocfind(sys_mL*sys_c);
+gain = dcgain(feedback(K*sys_c*sys_mL, -1, 1)); 
+
+% Controller coeff
+t_step = 0:0.01:20;
+hold off
+Ti = 1/z_c; 
+G_wok = K*(1+1/(Ti*s)); 
+[y_wok, t_wok] = step(1/gain*feedback(G_wok*sys_mL, 1), t_step); 
+
+figure; 
+hold on; 
+plot(t_wok, y_wok);
+yline(1, '--'); 
+hold off;
