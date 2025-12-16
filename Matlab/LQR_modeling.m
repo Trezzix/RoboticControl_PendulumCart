a9ec9126564fbde10013b00fed9ab2ad929df7fa
ThomasFHV <ThomaL2002@hotmail.com>
2025-12-16 12:48:31 +0100

    Trigger with LQR_modeling using imports from config_ip02.m

--- Matlab/LQR_modeling.m
+++ Matlab/LQR_modeling.m
@@ -13,60 +13,30 @@ XC_MAX = +XC_MINMAX;
 
 %% Constants
 
-%%%%%%%%%%%%%%%%%%%% Load conversions - Quanser %%%%%%%%%%%%%%%%%%%%%%
-% from radians to degrees
-K_R2D = 180 / pi;
-% from degrees to radians
-K_D2R = 1 / K_R2D;
-% from Inch to Meter
-K_IN2M = 0.0254;
-% from Meter to Inch
-K_M2IN = 1 / K_IN2M;
-% from rad/s to RPM
-K_RDPS2RPM = 60 / ( 2 * pi );
-% from RPM to rad/s
-K_RPM2RDPS = 1 / K_RDPS2RPM;
-% from oz-force to N
-K_OZ2N = 0.2780139;
-% from N to oz-force
-K_N2OZ = 1 / K_OZ2N;
-% From setup_ip02_sip.m
-K_EC = 2.274854409317804e-05;
-K_EP = 0.001533980787886;
-%%%%%%%%%%%%%%%%%%%% Load conversions - Quanser %%%%%%%%%%%%%%%%%%%%%%
-
+global ALPHA_MAX ALPHA_MIN IC_ALPHA0 K_EC K_EP VMAX_AMP IMAX_AMP
 %%%%%%%%%%%%%%%%%%%% Load parameters - Quanser %%%%%%%%%%%%%%%%%%%%%%
-R_m = 2.6; % [ohm] Motor armature resistance
-L_m = 180e-6; % [H] Motor armature Inductance
-k_t = 1.088 * K_OZ2N * K_IN2M; % [Nm/A] Motor torque constant
-eta_m = 1; % [ (Tm*w) / (Vm * Im) ] Motor electro-mechanical efficiency
-k_m = 0.804e-3 * K_RDPS2RPM; % [Vs/rad] Motor back-EMF constant
-J_m = 5.523e-5 * K_OZ2N * K_IN2M; % [kg*m^2] Rotor inertia
 M_c2 = 0.57; % [kg] IP02 Cart mass w/3 cable connectors
 M_w = 0.37; % [kg] Cart weight
-K_g = 3.71; % [-] Internal gear ratio (planetary gearbox)
-eta_g = 1; % [-] Internal gear ratio efficiency (planetary gearbox)
-N_mp = 24; % [-] Number of teeth: cart motor pinion
-r_mp = 0.5 / 2 * K_IN2M; % [m] Motor pinion radius
-N_pp = 56; % [-] Number of teeth: cart position pinion
-r_pp = 1.167 /2 * K_IN2M; % [m] Position pinion radius
-P_r = 1e-2 / 6.01; % [m/teeth] Rack pitch
-T_c = 0.814; % [m] Cart travel
-    % These are from reading SIP manual: Archiv/TechResources/UserManual
-        % Using long pendulum (_p = _pl)
 M_p = 0.23; % [kg] Pendulum: Mass
 l_p = 0.3302; % [m] Pendulum: from pivot to COM
 J_p = 7.88*10^-3; % [kg*m^2] Moment of Inertia about COM
 B_p = 0.0024; % [Nm*s/rad] Pendulum: Viscous Damping Coefficient
 g = 9.81; % [m/s^2] gravitational constant
+    %%% Type of Cart Load: set to 'NO_LOAD', 'WEIGHT' %%%
+    if weight
+        IP02_LOAD_TYPE = 'WEIGHT';
+        M_c = M_w + M_c2;
+        B_c = 5.4; % [Nm*s/rad]
+    else
+        IP02_LOAD_TYPE = 'NO_LOAD';
+        M_c = M_c2;
+        B_c = 4.3; % [Nm*s/rad]
+    end
+    %%% Amplifier Type: set to 'VoltPAQ' or 'Q3'%%%
+    AMP_TYPE = 'VoltPAQ';
+    % AMP_TYPE = 'Q3';
+[ R_m, J_m, k_t, eta_m, k_m, K_g, eta_g, M_c, r_mp, ~ ] = config_ip02( IP02_LOAD_TYPE, AMP_TYPE );
 %%%%%%%%%%%%%%%%%%%% Load parameters - Quanser %%%%%%%%%%%%%%%%%%%%%%
-if weight % with weight
-    M_c = M_w + M_c2;
-    B_c = 5.4; % [Nm*s/rad]
-else % without weight
-    M_c = M_c2;
-    B_c = 4.3; % [Nm*s/rad]
-end
 
 % Calculated
 B_eq = (k_m * eta_g * K_g^2 * eta_m * k_t) / (R_m * r_mp^2) + B_c; % Equivalent Damping
@@ -88,20 +58,15 @@ C_V = (eta_g * eta_m * K_g * k_t) / (R_m * r_mp);
 A = [ 0   0     1     0 ;
       0   0     0     1 ;
       0  A_32  A_33  A_34 ;  % x¨_c
-      0  A_42  A_43  A_44 ]; % α¨
+      0  A_42  A_43  A_44 ]  % α¨
 B = [                  0    ;
                        0    ;
       (J_p + M_p*l_p^2)*C_V/J_T ;
-              (M_p*l_p)*C_V/J_T ];
+              (M_p*l_p)*C_V/J_T ]
 C = [ 1 0 0 0 ;
-      0 1 0 0 ];
+      0 1 0 0 ]
 D = [ 0 ;
-      0 ];
-
-A
-B
-C
-D
+      0 ]
 
 %% Verify controllability
 
